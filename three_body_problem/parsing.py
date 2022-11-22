5383aa353fe30741b504f585d0e99c4bc0d7f091
grabt <grabt@uw.edu>
2022-11-21 18:56:41 -0800

    parsing file + initial cond

---
+++ three_body_problem/parsing.py
@@ -0,0 +1,22 @@
+import pydantic
+
+
+class Body(pydantic.BaseModel):
+    x: float
+    y: float
+    mass: float
+    v_x: float
+    v_y: float
+    color: str = "white"
+
+
+class Configuration(pydantic.BaseModel):
+    bodies: list[Body]
+    delta_t: float
+    video_length: int
+    output_path: str
+
+
+def parse_config(path: str):
+    config = Configuration.parse_file(path)
+    return config
