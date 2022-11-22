cc51ea4b364dbe6ac05fc24892b2bcd1714e4c9f
grabt <grabt@uw.edu>
2022-11-21 19:22:11 -0800

    defined contracts

---
+++ three_body_problem/physics.py
@@ -0,0 +1,12 @@
+from three_body_problem.parsing import Configuration, Body
+
+
+class Engine:
+    def __init__(self, configuration: Configuration) -> None:
+        pass
+
+    def simulate(self):
+        pass
+
+    def dataframe(self):
+        pass
---
+++ three_body_problem/visualizer.py
@@ -0,0 +1,10 @@
+from three_body_problem.parsing import Configuration,
+import pandas as pd
+
+
+class Visualizer():
+    def __init__(self, configuration: Configuration, dataframe: pd.DataFrame) -> None:
+        pass
+
+    def movie(self):
+        pass
