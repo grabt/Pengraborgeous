d0a4e71e7e0e740bbe66674f4b1ffb35fc9755a1
grabt <grabt@uw.edu>
2022-12-01 18:46:14 -0800

    Visualizer has solar system background via turtle

--- three_body_problem/visualizer.py
+++ three_body_problem/visualizer.py
@@ -1,5 +1,6 @@
 from three_body_problem.parsing import Configuration,
 import pandas as pd
+import turtle
 
 
 class Visualizer():
@@ -8,3 +9,33 @@ class Visualizer():
 
     def movie(self):
         pass
+
+
+class SolarSystemBody(turtle.Turtle):
+    def __init__(self, solar_system, mass, position=(0, 0), velocity=(0, 0), ):
+
+        super().__init__()
+        self.mass = mass
+        self.setpoition(position)
+        self.velocity = velocity
+
+        self.penup()
+        self.hideturtle()
+
+        solar_system.add_body(self)
+
+
+class SolarSystem:
+    def __init__(self, width, height):
+        self.solar_system = turtle.Screen()
+        self.solar_system.tracer(0)
+        self.solar_system.setup(width, height)
+        self.solar_system.bgcolor("black")
+
+        self.bodies = []
+
+    def add_body(self, body):
+        self.bodies.append(body)
+
+    def remove_body(self, body):
+        self.bodies.remove(body)
