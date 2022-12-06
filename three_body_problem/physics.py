62a336cab12621286bd4016af9b6f783e5b5d4c0
grabt <grabt@uw.edu>
2022-12-05 23:30:17 -0800

    orbits

--- three_body_problem/physics.py
+++ three_body_problem/physics.py
@@ -5,13 +5,54 @@ import numpy as np
 
 class Engine:
     def __init__(self, configuration: Configuration) -> None:
-        pass
+        self.video_length = configuration.video_length
+        self.delta_t = configuration.delta_t
+        self.bodies = configuration.bodies
+        self.data = []
 
     def simulate(self):
-        pass
+        t_0 = 0
+        t = t_0
+        dt = self.delta_t
+        t_end = self.video_length
+        big_G = 6.67e-11
+        while t < t_end:
+            for m1_idx in range(len(self.bodies)):
+                a_g = [0, 0]  # acceleration vector
+
+                for m2_idx in range(len(self.bodies)):
+                    if m1_idx != m2_idx:
+                        r_vector = [0, 0]
+                        r_vector[0] = self.bodies[m1_idx].x - \
+                            self.bodies[m2_idx].x
+                        r_vector[1] = self.bodies[m1_idx].y - \
+                            self.bodies[m2_idx].y
+
+                        r_mag = (r_vector[0]**2 + r_vector[1]**2)**0.5
+
+                        acceleration = -1.0 * big_G * \
+                            ((self.bodies[m2_idx].mass) / r_mag**2)
+                        r_unit_vector = [r_vector[0]/r_mag, r_vector[1]/r_mag]
+
+                        a_g[0] += acceleration * r_unit_vector[0]
+                        a_g[1] += acceleration * r_unit_vector[1]
+
+                self.bodies[m1_idx].v_x += a_g[0] * dt
+                self.bodies[m1_idx].v_y += a_g[1] * dt
+
+            for entity_idx in range(len(self.bodies)):
+                self.bodies[entity_idx].x += self.bodies[entity_idx].v_x * dt
+                self.bodies[entity_idx].y += self.bodies[entity_idx].v_y * dt
+            row_data = [t]
+            for entity_idx in range(len(self.bodies)):
+                row_data.append(self.bodies[entity_idx].x)
+                row_data.append(self.bodies[entity_idx].y)
+            self.data.append(row_data)
+            t += dt
 
     def get_dataframe(self):
-        data = []
-        for t in range(0, 10000):
-            data.append([t, np.cos(t/100), np.sin(t/100)])
-        return pd.DataFrame(data=data, columns=["time", "x_pos0", "y_pos0"])
+        columns = ["time"]
+        for entity_idx in range(len(self.bodies)):
+            columns.append("x_pos" + str(entity_idx))
+            columns.append("y_pos" + str(entity_idx))
+        return pd.DataFrame(data=self.data, columns=columns)
--- three_body_problem/visualizer.py
+++ three_body_problem/visualizer.py
@@ -11,30 +11,41 @@ class Visualizer():
         self.screen.bgcolor("black")
 
         self.screen.setup(configuration.width, configuration.height)
-        lower_x = dataframe["x_pos0"].min()
-        lower_y = dataframe["y_pos0"].min()
-        upper_x = dataframe["x_pos0"].max()
-        upper_y = dataframe["y_pos0"].max()
+
+        x_pos_data = []
+        y_pos_data = []
+        for column in dataframe.columns:
+            if column.startswith("x_pos"):
+                x_pos_data.append(column)
+            elif column.startswith("y_pos"):
+                y_pos_data.append(column)
+        lower_x = dataframe[x_pos_data].min().min()
+        lower_y = dataframe[y_pos_data].min().min()
+        upper_x = dataframe[x_pos_data].max().max()
+        upper_y = dataframe[y_pos_data].max().max()
         turtle.setworldcoordinates(lower_x, lower_y, upper_x, upper_y)
 
     def play_movie(self):
         turtles = []
         initial_conditions = self.dataframe.iloc[0]
-        #columns = self.dataframe.columns
-
-        t = turtle.Turtle()
-        turtles.append(t)
-        t.up()
-        t.shape("circle")
-        t.speed("slow")
-        t.turtlesize(1)
-        t.setposition(initial_conditions["x_pos0"],
-                      initial_conditions["y_pos0"])
-        t.color("blue", "blue")
-        t.down()
+        number_of_planets = len(self.configuration.bodies)
+        for planet_id in range(number_of_planets):
+            t = turtle.Turtle()
+            turtles.append(t)
+            t.up()
+            t.shape("circle")
+            t.speed("slow")
+            t.turtlesize(1)
+            t.setposition(initial_conditions["x_pos" + str(planet_id)],
+                          initial_conditions["y_pos" + str(planet_id)])
+            t.color(
+                self.configuration.bodies[planet_id].color, self.configuration.bodies[planet_id].color)
+            t.down()
 
         for i, cords in self.dataframe.iterrows():
-            t.setpos(cords["x_pos0"], cords["y_pos0"])
+            for planet_id in range(number_of_planets):
+                turtles[planet_id].setpos(cords["x_pos" + str(planet_id)],
+                                          cords["y_pos" + str(planet_id)])
 
     def add_body(self, body):
         self.bodies.append(body)
